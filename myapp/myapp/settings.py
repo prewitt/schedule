7521042f549719a735e4c4d30ea4f231267182e2
root <root@WayneZone.(none)>
2014-06-21 21:37:23 +0800

    pw edit setting

--- myapp/myapp/settings.py
+++ myapp/myapp/settings.py
@@ -154,6 +154,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'PIL',
     'app',
     'south',
     'staff',
